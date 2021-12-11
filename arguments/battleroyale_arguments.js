07898709fcf24e312b135f339ebdae95da59f487
Kevin Qiang <butterflybutterpot@gmail.com>
2021-12-11 12:16:34 +0900

    add arguments file for battle royale

---
+++ arguments/battleroyale_arguments.js
@@ -0,0 +1,13 @@
+const { BigNumber } = require('ethers')
+
+module.exports = [
+    'Nifty Royale X Tester: Nifty Royale NFT',
+    'TVBR',
+    BigNumber.from('1000000000000000').toNumber(),
+    5,
+    100,
+    'QmPS3DjUdXZAFXq3SgDPqHapnqQqWqd25VX87Ri4dWTkxE',
+    'Qmdy4U4V9JHoFgN4uQ7st9ZDrN7KGf2bZ6fe99kw8kASrQ',
+    'https://niftyroyale.mypinata.cloud/ipfs/',
+    BigNumber.from('2556100800').toNumber()
+  ];
