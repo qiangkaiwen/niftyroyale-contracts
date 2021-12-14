8f9fcc24f425fb02ceeced1532e39b789c7932ce
Kevin Qiang <butterflybutterpot@gmail.com>
2021-12-15 02:28:43 +0900

    change toNumber into toBigInt

--- scripts/deploy_ChainlinkBattle.js
+++ scripts/deploy_ChainlinkBattle.js
@@ -12,7 +12,7 @@ async function main() {
     '0x8C7382F9D8f56b33781fE506E897a4F1e2d17255', // Chainlink VRF Coordinator address
     '0x326C977E6efc84E512bB9C30f76E30c160eD06FB', // LINK token address
     '0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4', // Key Hash
-    BigNumber.from('100000000000000').toNumber() // Fee
+    BigNumber.from('100000000000000').toBigInt() // Fee
   )
 
   console.log('BattleRoyale deployed to:', chainlinkBattle.address)
