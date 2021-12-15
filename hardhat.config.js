aa8374db6d5ee462316e77aabee7f85eead2d158
Kevin Qiang <butterflybutterpot@gmail.com>
2021-12-16 06:05:42 +0900

    add condition on performUpKeep

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,38 +1,40 @@
-require('dotenv').config()
+require("dotenv").config()
 
-require('@nomiclabs/hardhat-etherscan')
-require('@nomiclabs/hardhat-waffle')
-require('hardhat-gas-reporter')
-require('solidity-coverage')
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.6',
+  solidity: "0.8.6",
   networks: {
     mainnet: {
-      url: process.env.MAINNET_URL || '',
+      url: process.env.MAINNET_URL || "",
       gasPrice: 60000000000,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     rinkeby: {
-      url: process.env.RINKEBY_URL || '',
+      url: process.env.RINKEBY_URL || "",
       gasPrice: 60000000000,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     mumbai: {
-      url: process.env.MUMBAI_URL || '',
+      url: process.env.MUMBAI_URL || "",
+      gasPrice: 150000000000,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     matic: {
-      url: process.env.MATIC_URL || '',
+      url: process.env.MATIC_URL || "",
+      gasPrice: 150000000000,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
-    currency: 'USD',
+    currency: "USD",
   },
   etherscan: {
     apiKey: process.env.POLYGONSCAN_API_KEY,
