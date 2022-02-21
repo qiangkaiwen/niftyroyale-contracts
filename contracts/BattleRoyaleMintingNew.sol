38f92d22d77ceff8432a7c6366758f9d0600d65d
Kevin Qiang <butterflybutterpot@gmail.com>
2022-02-22 04:49:02 +0800

    add IERC20 to ethereum contracts

--- contracts/BattleRoyaleMintingNew.sol
+++ contracts/BattleRoyaleMintingNew.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
