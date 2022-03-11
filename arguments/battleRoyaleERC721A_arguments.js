6dcfd5347ead0e0d460ac27024eb46d7539d9dd0
Kevin Qiang <butterflybutterpot@gmail.com>
2022-03-11 22:48:56 +0800

    create script and argument file for erc721a battle

---
+++ arguments/battleRoyaleERC721A_arguments.js
@@ -0,0 +1,16 @@
+const { BigNumber } = require("ethers")
+
+// max token limit per wallet in public sale
+// max token limit per wallet in presale
+// max token numbers for team
+module.exports = [
+  "Nifty Royale X Tester: Nifty Royale NFT BattleRoyaleERC721A",
+  "TVNRNB",
+  5,
+  3,
+  100,
+  30,
+  BigNumber.from("20000000000000").toBigInt(),
+  10,
+  10,
+]
---
+++ scripts/deploy_BattleRoyaleERC721A.js
@@ -0,0 +1,32 @@
+const hre = require("hardhat")
+const { BigNumber } = require("ethers")
+
+async function main() {
+  const [deployer] = await ethers.getSigners()
+
+  console.log("Deployer address:", deployer.address)
+  console.log("Deployer balance:", (await deployer.getBalance()).toString())
+
+  const BattleRoyaleERC721A = await hre.ethers.getContractFactory("BattleRoyaleERC721A")
+
+  const battleRoyaleERC721A = await BattleRoyaleERC721A.deploy(
+    "Nifty Royale X Tester: Nifty Royale NFT BattleRoyaleERC721A",
+    "TVNRNB",
+    5,
+    3,
+    100,
+    30,
+    BigNumber.from("20000000000000").toBigInt(),
+    10,
+    10
+  )
+
+  console.log("Transaction Hash:", battleRoyaleERC721A.deployTransaction.hash)
+  await battleRoyaleERC721A.deployed()
+  console.log("Contract Address:", battleRoyaleERC721A.address)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
