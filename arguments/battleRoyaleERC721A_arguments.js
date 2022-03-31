cf33af083e9cc28ba8999eb0ad40648108f8c4b1
Kevin Qiang <butterflybutterpot@gmail.com>
2022-04-01 02:44:29 +0800

    add comments for argument

--- arguments/battleRoyaleERC721A_arguments.js
+++ arguments/battleRoyaleERC721A_arguments.js
@@ -3,11 +3,11 @@ const { BigNumber } = require("ethers")
 module.exports = [
   "Nifty Royale X Tester: ERC721A Test",
   "TVNRET",
-  5,
-  3,
-  10,
-  31,
-  BigNumber.from("20000000000000").toBigInt(),
-  10,
-  10,
+  5, //maximum number of tokens per transaction in public sale
+  3, //maximum number of tokens per transaction in presale sale
+  10, //maximum supply for internal minting
+  31, // maximum supply
+  BigNumber.from("20000000000000").toBigInt(), // price
+  10, //maximum number of tokens per wallet in public sale
+  10, //maximum number of tokens per wallet in presale sale
 ]
